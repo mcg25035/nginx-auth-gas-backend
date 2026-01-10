35279c5a577c63f5f7f846b699eb63a9be604728
CodingBear <70533278+mcg25035@users.noreply.github.com>
2026-01-11 02:20:33 +0800

    chore: Delete Secret.js containing Discord webhook URL.

--- Secret.js
+++
@@ -1,6 +0,0 @@
-// ----------------------------------------------------
-// Secrets (Ignored by Git)
-// ----------------------------------------------------
-
-// Discord Webhook URL
-const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1458814909948100693/nVoFOIkWmCxdFuGsFaCR6IrKS3n5h7YOX_BNLUWy1Yf6djnnFQSSTg1Y-cjE6_LjBpc9";
